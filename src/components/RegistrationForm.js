5de7be92a9e0a057c0569b8f8acd246eb1b2e343
Adam N <adam.nawroc@gmail.com>
2019-01-02 14:57:24 +0000

    progress

---
+++ src/components/RegistrationForm.js
@@ -0,0 +1,5 @@
+export class RegistrationForm extends React.Component {
+  render() {
+    return <div>I am one line, so I do not need the parentheses</div>
+  }
+}
---
+++ src/components/Webpage.js
@@ -0,0 +1,5 @@
+export class Webpage extends React.Component {
+  render() {
+    return <div>I am one line, so I do not need the parentheses</div>
+  }
+}
